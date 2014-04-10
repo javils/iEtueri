4dd3dc7b6eda431c5d3db3d66fbdf2badf97888a
Javils <javiluke93@gmail.com>
2014-04-10 14:02:18 +0200

    Add layout for today Add Fragment for control

---
+++ src/today/TodayFragment.java
@@ -0,0 +1,51 @@
+package today;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import schedule.Event;
+import schedule.EventsManager;
+import schedule.ScheduleTodayAdapter;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.javils.ietueri.R;
+
+/** This class if for Today View */
+public class TodayFragment extends Fragment {
+
+	/** Views */
+	private ListView listEvents;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_today, container, false);
+
+		listEvents = (ListView) view.findViewById(R.id.list_today_today);
+
+		Calendar now = Calendar.getInstance();
+		int year = now.get(Calendar.YEAR);
+		int month = now.get(Calendar.MONTH);
+		int day = now.get(Calendar.DAY_OF_MONTH);
+
+		if (EventsManager.isThreadfinish()) {
+			ScheduleTodayAdapter adapter = new ScheduleTodayAdapter(view.getContext(), R.layout.schedule_list_item,
+					new ArrayList<Event>());
+
+			EventsManager.setAdapter(adapter);
+
+			ArrayList<Event> events = EventsManager.find(year, month, day);
+			if (events != null)
+				EventsManager.getAdapter().setItems(events);
+
+			EventsManager.getAdapter().notifyDataSetChanged();
+			listEvents.setAdapter(EventsManager.getAdapter());
+		}
+
+		return view;
+	}
+}
