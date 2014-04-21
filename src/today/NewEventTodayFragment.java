720264d788a4b1c27e63f3f2a45c5ca8e303951b
Javils <javiluke93@gmail.com>
2014-04-21 15:31:38 +0200

    Fix the view of newEvent

--- src/today/NewEventTodayFragment.java
+++ src/today/NewEventTodayFragment.java
@@ -11,7 +11,7 @@ import com.javils.ietueri.R;
 public class NewEventTodayFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+		View view = inflater.inflate(R.layout.fragment_new_event_today, container, false);
 		return view;
 	}
 }
