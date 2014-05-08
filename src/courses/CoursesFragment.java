341e798ced5360992b1d2159d4a47a7cca14429d
Javils <javiluke93@gmail.com>
2014-05-08 16:45:11 +0200

    Show the Detail view when click in the Course item and send the course data.

--- src/courses/CoursesFragment.java
+++ src/courses/CoursesFragment.java
@@ -6,15 +6,20 @@ import navigationdrawer.MainActivity;
 import navigationdrawer.NavigationDrawerController;
 import utility.DatabaseContract;
 import utility.DatabaseHelper;
+import utility.OnClickButtonXml;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.javils.ietueri.R;
 
@@ -32,13 +37,7 @@ public class CoursesFragment extends Fragment {
 		listCourses = (ListView) view.findViewById(R.id.list_courses);
 		ArrayList<Course> courses = new ArrayList<Course>();
 		courses = getCoursesInDB();
-		// Examples before read the database
-		/*
-		 * courses.add(new Course("1º Semestre", 3, 4.5, "24/5/2013"));
-		 * courses.add(new Course("2º Semestre", 4, 5.5, "25/6/2013"));
-		 * courses.add(new Course("3º Semestre", 5, 6.5, "26/7/2013"));
-		 * courses.add(new Course("4º Semestre", 6, 7.5, "27/8/2013"));
-		 */
+
 		CoursesAdapter adapter;
 		if (courses != null) {
 			adapter = new CoursesAdapter(getActivity(), R.layout.courses_list_item, courses);
@@ -46,6 +45,29 @@ public class CoursesFragment extends Fragment {
 			listCourses.setAdapter(adapter);
 		}
 
+		listCourses.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				TextView textName = (TextView) view.findViewById(R.id.courses_list_item_course_name);
+				TextView textNSubjects = (TextView) view.findViewById(R.id.courses_list_item_number_subjects);
+				TextView textAverage = (TextView) view.findViewById(R.id.courses_list_item_course_average);
+
+				String subjectName = textName.getText().toString();
+				int numberOfSubjects = Integer.parseInt(textNSubjects.getText().toString().split(" ")[0]);
+				double average = Double.parseDouble(textAverage.getText().toString());
+
+				Course course = getCourseInDB(subjectName, numberOfSubjects, average);
+
+				FragmentManager fragmentManager = getFragmentManager();
+				Fragment newFragment = NavigationDrawerController
+						.newInstance(NavigationDrawerController.SECTION_NUMBER_DETAIL_COURSE);
+				if (newFragment instanceof OnClickButtonXml)
+					MainActivity.setOnClickFragment(newFragment);
+				((CourseDetailFragment) newFragment).setCourse(course);
+				fragmentManager.beginTransaction().replace(R.id.navigation_drawer_container, newFragment).commit();
+			}
+		});
 		return view;
 	}
 
@@ -74,6 +96,33 @@ public class CoursesFragment extends Fragment {
 		}
 
 		return result;
+	}
+
+	private Course getCourseInDB(String name, int numberOfSubjects, double average) {
+		dbHelper = new DatabaseHelper(getActivity());
+		db = dbHelper.getReadableDatabase();
+
+		String[] projection = { DatabaseContract.Courses._ID, DatabaseContract.Courses.COLUMN_NAME_INIT_DATE,
+				DatabaseContract.Courses.COLUMN_NAME_END_DATE, DatabaseContract.Courses.COLUMN_NAME_SUBJECTS_IDS };
+		String selection = DatabaseContract.Courses.COLUMN_NAME_COURSE_NAME + "=?  AND "
+				+ DatabaseContract.Courses.COLUMN_NAME_NUMBER_OF_SUBJECTS + "=? AND "
+				+ DatabaseContract.Courses.COLUMN_NAME_AVERAGE + "=? ";
+		String[] args = { name, String.valueOf(numberOfSubjects), String.valueOf(average) };
+
+		Cursor cur = db.query(DatabaseContract.Courses.TABLE_NAME, projection, selection, args, null, null,
+				DatabaseContract.Courses._ID + " DESC");
+
+		cur.moveToFirst();
+
+		int id = cur.getInt(cur.getColumnIndexOrThrow(DatabaseContract.Courses._ID));
+		String initDate = cur.getString(cur.getColumnIndexOrThrow(DatabaseContract.Courses.COLUMN_NAME_INIT_DATE));
+		String endDate = cur.getString(cur.getColumnIndexOrThrow(DatabaseContract.Courses.COLUMN_NAME_END_DATE));
+		String subjectsIds = cur
+				.getString(cur.getColumnIndexOrThrow(DatabaseContract.Courses.COLUMN_NAME_SUBJECTS_IDS));
+
+		Course newCourse = new Course(id, name, numberOfSubjects, average, initDate, endDate, subjectsIds);
+
+		return newCourse;
 	}
 
 	@Override
